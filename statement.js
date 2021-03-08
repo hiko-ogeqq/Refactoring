f8a7a2854735a331b1fb1d54aba81497dc08223c
hiko-ogeqq <knhk0419@gmail.com>
2021-03-09 03:03:02 +0900

    END add renderHtml

--- statement.js
+++ statement.js
@@ -4,7 +4,25 @@ exports.statement = (invoice, plays) => {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
+exports.htmlStatement = (invoice, plays) => {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
 function renderPlainText(data){
+  let result = `Statement for ${data.customer}\n`;
+  
+  for(let perf of data.performances){
+    // 注文の内訳を出力
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+  }
+
+  result += `Amount owned is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
+  return result;
+}
+
+// TODO: HTML版 ちゃんとする
+function renderHtml(data){
   let result = `Statement for ${data.customer}\n`;
   
   for(let perf of data.performances){
--- test/test.js
+++ test/test.js
@@ -1,17 +1,28 @@
 const assert = require('assert');
-const js = require("../statement.js");
+const testTarget = require("../statement.js");
 describe("印字テスト", () => {
   let invoices = require("../invoices.json");
   let plays = require("../plays.json");
   
-  it("it", () => {
-    var expectedResult = "Statement for BigCo\n"
+  it("plain text", () => {
+    let expectedResult = "Statement for BigCo\n"
                       + "  Hamlet: $650.00 (55 seats)\n"
                       + "  As You Like It: $580.00 (35 seats)\n"
                       + "  Othello: $500.00 (40 seats)\n"
                       + "Amount owned is $1,730.00\n"
                       + "You earned 47 credits\n";
     
-    assert.equal(js.statement(invoices[0], plays), expectedResult);
+    assert.equal(testTarget.statement(invoices[0], plays), expectedResult);
+  });
+
+  it("Html", () => {
+    let expectedResult = "Statement for BigCo\n"
+                       + "  Hamlet: $650.00 (55 seats)\n"
+                       + "  As You Like It: $580.00 (35 seats)\n"
+                       + "  Othello: $500.00 (40 seats)\n"
+                       + "Amount owned is $1,730.00\n"
+                       + "You earned 47 credits\n";
+
+    assert.equal(testTarget.htmlStatement(invoices[0], plays), expectedResult);
   });
 });
